880ba66883da863bbcb7f2b886b714f7c6b1d40d
Kevin Hayes <khayes@naperville203.org>
2020-12-16 09:46:55 -0600

    Initial commit

---
+++ Intro to Objects - starter/ch4notes.java
@@ -0,0 +1,52 @@
+/********************************************************************
+// These are the notes for ch 4 used in hayes class
+//******************************************************************* */
+
+
+import java.text.DecimalFormat;
+import java.text.NumberFormat;
+
+import javax.swing.*;
+
+public class ch4notes
+{
+
+ //static int sum = 987654;
+
+public static void main (String[] args)
+{
+
+    mathy solver = new mathy();
+
+    System.out.println(solver.abs(-5));
+    System.out.println(abs(-8));
+
+/**/
+}// end of main
+
+/**
+    take the absoulute value of the number
+    @param int value
+    @return absoulute value of number
+*/
+
+/**
+    take the absoulute value of the number
+    @param double value
+    @return absoulute value of number
+*/
+
+
+
+
+
+}// end of class
+
+
+
+
+
+
+
+
+
---
+++ Intro to Objects - starter/mathy.java
@@ -0,0 +1,31 @@
+import java.text.DecimalFormat;
+import java.text.NumberFormat;
+
+//import javax.swing.*;
+
+public class mathy
+{
+    private int area=0;
+
+    /**
+        default constructor - stubed in
+    */
+    public mathy( )
+    {
+
+        System.out.println ("hi from default constructor" );
+
+    }//end of default const
+
+
+
+
+
+    public String toString()
+    {
+        return ("Hi what do you want?, I'm just Mathy");
+
+    }
+
+
+}//end of class
